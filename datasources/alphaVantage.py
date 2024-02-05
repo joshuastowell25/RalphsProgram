15f212bca9df73dee134b278d49f0761a23536ee
Josh Stowell <joshua.stowell@gmail.com>
2024-02-05 08:48:00 -0600

    updates

--- datasources/alphaVantage.py
+++ datasources/alphaVantage.py
@@ -3,10 +3,12 @@ from typing import List
 import requests
 from app_secrets import app_secrets
 from datetime import datetime
-#interval = '5min'
-def getDatapoints(symbol, interval):
+#interval = one of: '1min','5min', '15min','30min','60min'
+#month = '2009-01'
+def getDatapoints(symbol, interval='1min', month = None):
     datapoints: List[Datapoint] = []
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&outputsize=full&apikey={app_secrets.ALPHA_VANTAGE_API_KEY}"
+    monthQueryParam = f"&month={month}" if month else ""
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY{monthQueryParam}&symbol={symbol}&interval={interval}&outputsize=full&apikey={app_secrets.ALPHA_VANTAGE_API_KEY}"
     r = requests.get(url)
     data = r.json()
 
@@ -20,6 +22,24 @@ def getDatapoints(symbol, interval):
         datapoints.append(Datapoint(dt, price))
 
     return datapoints
+
+#gets all the 1 minute data that alpha vantage has for the asset
+#free tier is 5reqs/min and max 100 reqs/day
+#paid tier has many levels: https://www.alphavantage.co/premium/
+#highest tier is 1200 reqs/min which would mean 10 years worth of 1min data can be requested per minute
+#lowest tier is 30reqs/min aka 1 req per every 2 seconds
+def getAllDatapoints(symbol, maxReqsPerMin = 30):
+    months = list(range(1,13))
+    datapoints: List[Datapoint] = []
+    year = 2000
+    month = 1
+
+    datapoints.extend(getDatapoints(symbol, '1min', month=f"{year}-{month:02d}"))
+
+    return datapoints
+
+def saveDatapointsToFileAsJson():
+    pass
 
 def get_data():
     symbol = input("What is the symbol of the data you want to use?\n")
